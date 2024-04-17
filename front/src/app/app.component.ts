d7bcbae72abe5071b2c93e9ace443804d9bdfe20
Sid-Athos <sa.bennaceur@gmail.com>
2024-04-17 03:44:17 +0200

    PET-25 removed check

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {CreateCaregiverComponent} from "./create-caregiver/create-caregiver.component";
+import {CreateCaregiverComponent} from "./components/create-caregiver/create-caregiver.component";
 
 @Component({
   selector: 'app-root',
