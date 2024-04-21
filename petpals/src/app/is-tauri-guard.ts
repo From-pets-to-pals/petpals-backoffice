91b104cdc8ee1e47bf0cb504e60bb1fb94a54a76
Sid-Athos <sa.bennaceur@gmail.com>
2024-04-21 23:28:56 +0200

    PET-41 removed useless arguments

--- petpals/src/app/is-tauri-guard.ts
+++ petpals/src/app/is-tauri-guard.ts
@@ -1,7 +1,7 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
-export const isTauriGuard: CanActivateFn = (route, state) => {
+export const isTauriGuard: CanActivateFn = () => {
   const router: Router = inject(Router);
 
   const hasRole: boolean = window.__TAURI__ !== undefined;
