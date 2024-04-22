3403fd8e1cba20e93073c0a5ef955dc6314c5fb8
Sid-Athos <sa.bennaceur@gmail.com>
2024-04-22 02:08:50 +0200

    PET-41 fix CSS bug with tailwind addition, routing done

--- petpals/src/app/app.routes.ts
+++ petpals/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import {CreateCaregiverComponent} from "./components/create-caregiver/create-caregiver.component";
 import {TauriComponent} from "./components/tauri/tauri.component";
-import {isTauriGuard} from "./is-tauri-guard";
+import {isTauriGuard} from "./guards/is-tauri-guard";
 
 export const routes: Routes = [
 	{path: 'create-caregiver', component: CreateCaregiverComponent},
